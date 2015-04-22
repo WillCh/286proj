9af9a0b983c3302e9014c94bb34366cb06ffdf5d
Enrico Tanuwidjaja <mincoss@hotmail.com>
2015-04-21 22:39:40 -0700

    Added namespace, restructured classes, prepared to integrate

--- repo/MetadataRepo/src/main/java/edu/berkeley/MetadataRepo/Main.java
+++ repo/MetadataRepo/src/main/java/edu/berkeley/MetadataRepo/Main.java
@@ -9,53 +9,15 @@ public class Main
 {
     public static void main(String[] args)
     {
+        MetadataRepo repo = new MetadataRepo("54.69.1.154");
         System.out.println("Welcome to the Metadata Repo shell!");
         System.out.print("> ");
         Scanner scan = new Scanner(System.in);
         while (scan.hasNext())
         {
-            parseCommand(scan.nextLine());
+            repo.execute(scan.nextLine());
             System.out.print("> ");
         }
     }
 
-    private static void parseCommand(String line)
-    {
-        if (line.length() == 0)
-            return;
-
-        String[] cmds = line.split(" ");
-        String act =  cmds[0];
-
-        try
-        {
-            if (act.equals("commit"))
-            {
-                DatabaseController.commit(cmds[1], cmds[2]);
-                System.out.println("Commit successful");
-            }
-            else if (act.equals("dump"))
-            {
-                DatabaseController.dump();
-            }
-            else if (act.equals("show"))
-            {
-                DatabaseController.show(cmds[1]);
-            }
-            else if (act.equals("clear"))
-            {
-                DatabaseController.clear();
-                System.out.println("Repo has been cleared");
-            }
-            else
-            {
-                System.out.println("Error: Unrecognized command");
-            }
-        }
-        catch (Exception e)
-        {
-            System.out.println("Error: Syntax error in command");
-            System.out.println(e.toString());
-        }
-    }
 }
