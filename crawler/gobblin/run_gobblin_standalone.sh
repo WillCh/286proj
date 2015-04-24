b158624ec6577a92a130b4538a5909c2d058d16f
Ian Juch <ianjuch@berkeley.edu>
2015-04-24 12:00:30 -0700

    modify gobblin run script to set environment vars instead of using command line flags. the environment vars are necessary to stop gobblin-standalone using 'gobblin-standalone.sh stop'

--- crawler/gobblin/run_gobblin_standalone.sh
+++ crawler/gobblin/run_gobblin_standalone.sh
@@ -4,9 +4,14 @@
 # Command to run gobblin standalone example after compilation
 
 # clear out old test_workdir
-rm -rf test_workdir/*
+rm -rf test_workdir
+mkdir test_workdir
+
+# set environment variables
+export GOBBLIN_JOB_CONFIG_DIR=../test_config
+export GOBBLIN_WORK_DIR=../test_workdir
 
 # run gobblin
 cd gobblin-dist/
-bin/gobblin-standalone.sh start --workdir ../test_workdir --conf ../test_config
+bin/gobblin-standalone.sh start #--workdir ../test_workdir --conf ../test_config
 
