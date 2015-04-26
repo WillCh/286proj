0e8af1b953dac468aef2491081d59050fcedfde7
Ian Juch <ianjuch@berkeley.edu>
2015-04-25 17:38:00 -0700

    add simple Gobblin wrapper for the crawler. incomplete, see TODO's

---
+++ crawler/config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+#-----------------------------------------------------------------
+# @author: ianjuch
+# @file: config.py
+# @description: crawler config file
+#-----------------------------------------------------------------
+
+#special_filetypes=".csv"
+
+#stats_to_collect="date","time","size"
+
+directories_to_crawl=["gobblin/test_source"]
