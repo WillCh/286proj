149424e4f2b2122663a8032f92285c8d72e2bb49
EC2 Default User <ec2-user@ip-172-31-37-144.us-west-2.compute.internal>
2015-05-02 02:06:52 +0000

    add script to monitor incoming landing directory for tar files from crawler

---
+++ dataMover/inotifywait.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# @file:inotifywait.sh
+# @description: monitors specified directory (recursively)
+#   for new files
+# @usage:
+#   ./inotifywait.sh /path/to/file scp_target_location
+
+inotifywait -m "kafka/incoming" --format '%w%f' -e create |
+  while read file; do
+    ./action.sh $file
+  done
